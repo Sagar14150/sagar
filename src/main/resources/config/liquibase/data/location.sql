339b865ddc8b1c1cf70ff6deebe02c0eff4a5a9c
shinde_sagar <sagar.shinde@pratititech.com>
2024-05-26 18:47:55 +0530

    ssss

---
+++ src/main/resources/config/liquibase/data/location.sql
@@ -0,0 +1,5 @@
+CREATE TABLE sanjuwels.location (
+	user_id bigint NOT NULL,
+	user_name varchar(100) NOT NULL,
+	location varchar(500)
+);
---
+++ src/main/resources/config/liquibase/data/user.sql
@@ -0,0 +1,5 @@
+CREATE TABLE sanjuwels.user (
+	user_id bigint NOT NULL,
+	user_name varchar(100) NOT NULL,
+	location varchar(500)
+);
